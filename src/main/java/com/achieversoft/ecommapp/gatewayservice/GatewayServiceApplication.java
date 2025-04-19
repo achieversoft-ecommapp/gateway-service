ef25c7720406b01e07f7f9e6f1b24d3ceea5d398
prsgithub <ranjansahoo.prakash@gmail.com>
2025-04-19 13:40:23 +0530

    initial commit

--- src/main/java/com/achieversoft/ecommapp/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/achieversoft/ecommapp/gatewayservice/GatewayServiceApplication.java
@@ -1,13 +1,13 @@
-package com.achieversoft.ecommapp.apigateway;
+package com.achieversoft.ecommapp.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
 }
